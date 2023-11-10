d16272ba62d15f1e2cf94a065316c9f9672803db
Daniyar Nasirov <lilbonnie666@gmail.com>
2023-11-10 17:41:56 +0600

    changed the activity selector to a checkbox

--- src/Сomponents/UserForm/UserForm.tsx
+++ src/Сomponents/UserForm/UserForm.tsx
@@ -1,93 +1,96 @@
-import {User} from '../../Types';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { User } from '../../Types';
 
 interface Props {
-  onSubmit: (user: User) => void;
+    onSubmit: (user: User) => void;
 }
 
-const UserForm: React.FC<Props> = ({onSubmit}) => {
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    isActive: '',
-    role: 'User',
-  });
-
-  const changeUser = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-
-    setUser(prev => ({
-      ...prev,
-      [e.target.name]: e.target.value
-    }));
-  };
-
-  const onFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSubmit({
-      id: Math.random().toString(),
-      ...user,
-      isActive: user.isActive.toString(),
+const UserForm: React.FC<Props> = ({ onSubmit }) => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        activity: false,
+        role: 'User',
     });
-  };
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value, type } = e.target;
 
-  return (
-    <form onSubmit={onFormSubmit}>
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="form-control"
-          value={user.name}
-          onChange={changeUser}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description">Email:</label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          className="form-control"
-          value={user.email}
-          onChange={changeUser}
-          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
-          required
-        />
+        if (type === 'checkbox') {
+            const isChecked = (e.target as HTMLInputElement).checked;
+            setFormData(prev => ({
+                ...prev,
+                [name]: isChecked,
+            }));
+        } else {
+            setFormData(prev => ({
+                ...prev,
+                [name]: value,
+            }));
+        }
+    };
 
-      </div>
-      <div className="form-group">
-        <label htmlFor="isActive">Active:</label>
-        <select
-          name="isActive"
-          className="form-control"
-          value={user.isActive}
-          onChange={changeUser}
-        >
-          <option value="true">True (online)</option>
-          <option value="false">False (offline)</option>
-        </select>
-      </div>
+    const onFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        onSubmit({
+            id: Math.random().toString(),
+            ...formData,
+        });
+    };
 
-      <div className="form-group">
-        <label htmlFor="role">Role</label>
-        <select
-          name="role"
-          className="form-control"
-          placeholder="select role"
-          value={user.role}
-          onChange={changeUser}>
-          <option value="user">User</option>
-          <option value="editor">Editor</option>
-          <option value="admin">Administrator</option>
-        </select>
-      </div>
-
-      <button type="submit" className="btn btn-primary mt-2">Add</button>
-    </form>
-  );
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="form-control"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    className="form-control"
+                    value={formData.email}
+                    onChange={handleChange}
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
+                    required
+                />
+            </div>
+            <div className="mb-2 form-check my-3 mx-1">
+                <label htmlFor="checkbox" className="form-check-label fs-6">Active
+                <input
+                    id="checkbox"
+                    type="checkbox"
+                    className="form-check-input fs-6"
+                    name='activity'
+                    checked={formData.activity}
+                    onChange={handleChange}
+                /></label>
+            </div>
+            <div className="form-group">
+                <label htmlFor="role">Role</label>
+                <select
+                    name="role"
+                    className="form-control"
+                    value={formData.role}
+                    onChange={handleChange}
+                >
+                    <option value="user">User</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Administrator</option>
+                </select>
+            </div>
+            <button type="submit" className="btn btn-primary mt-2">Add</button>
+        </form>
+    );
 };
 
 export default UserForm;
--- src/Сomponents/Users/UserItem.tsx
+++ src/Сomponents/Users/UserItem.tsx
@@ -6,15 +6,14 @@ interface Props {
 }
 
 const UserItem: React.FC<Props> = ({ user }) => {
-
     return (
         <div className="card mb-2 col-10 mx-5 bg-primary">
             <div className="row no-gutters">
                 <div className="col-sm-10 ">
                     <div className="card-body mx-4 fs-6 text-light">
                         <h5 className="card-title">{user.name}</h5>
-                        <p className="card-text small">{user.email}</p>
-                        <p className="card-text small">Now: {user.isActive ? 'Offline' : 'Online'}</p>
+                        <p className="card-text small">Email: {user.email}</p>
+                        <p className="card-text small">Active: {!user.activity ? 'Offline' : 'Online'}</p>
                         <p className="card-text small">Role: {(user.role)}</p>
                     </div>
                 </div>
